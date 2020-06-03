7a50bdb92185be9f46a31f6a582346ee53b76eb2
Koray <koraym@gmail.com>
2020-06-03 02:36:25 -0700

    Readme Update, some comments

--- Readme.md
+++ Readme.md
@@ -1,2 +1,4 @@
+[![Solid](https://www.ohshiftlabs.com/assets/images/logo.png)](https://www.ohshiftlabs.com/)
+
 # Express Object Router
-This library allows you to manage pages with a simple map object for express servers.g
+This library allows you to manage pages with a simple map object for express servers.
--- route.js
+++ route.js
@@ -2,45 +2,58 @@ module.exports = {
 
     defaults:{
         headers:(req,res)=>{//Required
+            //Will be called before routing.
             res.setHeader('X-Powered-By', 'OhShift Web Server');
         },
 
         notfound:404, //Required
 
         defaultCallback:(req,res,processedPath)=>{
+            //Optional, will be called before each routing with processed path value.
             console.log(`processedPath ${processedPath}`)
         }
     },
 
     pages: {
         homepage:(req,res)=>{
+            //Default Landing Page
+
             let pageHeader = `<center style="font-family:arial"><h1>OhShift</h1></center>`
             let pageFooter = ""
             res.send(`${pageHeader} Hello This is home page. ${pageLinks()}${pageFooter}`)
         },            
     
         notfound:(req, res)=>{
+            //Default 404 Page.
             res.send(`This is 404 Page ${pageLinks()}`);
         },
     
         level1:{
-            default:(req, res)=>{
+            //Example root page
+            default:(req, res)=>{//Default Page of the object/
                 res.send(`Level 1 Default Page ${pageLinks()}`)
             },
-            level2:(req,res)=>{
-                res.send(`Level2 Page ${pageLinks()}`)
 
+            level2:(req,res)=>{
+                //Level2 page is a child of level1.
+                res.send(`Level2 Page ${pageLinks()}`)
+            },
+
+            "test.html":(req, res)=>{
+                //Level2 Test.html page is a child of level1.
+                res.send(`Test.html ${pageLinks()}`)
             }
             
         }
     }
 }
 
-let pageLinks = () => {
+let pageLinks = () => {//Demo Content
     let html= "<br>"
     html+= '<a href="/">Home Page</a><br>'
     html+= '<a href="/level1">Level 1 Page</a><br>'
     html+= '<a href="/level1/level2">Level 2 Page</a><br>'
+    html+= '<a href="/level1/test.html">Level 2 Page Test.HTML</a><br>'
 
     return html;
 }
